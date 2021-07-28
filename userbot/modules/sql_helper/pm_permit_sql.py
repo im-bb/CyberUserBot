990e3161a8a8c0c66abb4aaae742441b99f10eb5
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-07-28 20:32:25 +0400

    Update pm_permit_sql.py

--- userbot/modules/sql_helper/pm_permit_sql.py
+++ userbot/modules/sql_helper/pm_permit_sql.py
@@ -37,3 +37,9 @@ def dissprove(chat_id):
     if rem:
         SESSION.delete(rem)
         SESSION.commit()
+        
+
+def cyberizaceler():
+    rem = SESSION.query(PMPermit).all()
+    SESSION.close()
+    return rem 
