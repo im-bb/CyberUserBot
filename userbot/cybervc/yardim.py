7bfa06fe6a1629f4b69702f0bb07f976ca2773fb
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-08-06 23:12:35 +0400

    Update yardim.py

--- userbot/cybervc/yardim.py
+++ userbot/cybervc/yardim.py
@@ -7,7 +7,7 @@ class Text:
     admin_deyilsen = "`Bunu etmək üçün icazəniz yoxdur!`"
     yukle = "`Yüklənir...`"
     yardim = """
-**Available Commands:**\n
+**Əmrlər:**\n
   - `{x}alive` - __asistanın aktiv olub olmadığını yoxlayın.__
   - `{x}seslendir <link/mp3-ə cavab>` - __musiqini səsləndirər.__
   - `{x}pauza` - __pauza edər.__
@@ -15,10 +15,8 @@ class Text:
   - `{x}song <mahnı adı>` - __musiqini yükləyər.__
 **CYBER USERBOT**"""
 
-
-    
 async def mahniniseslendir(pycalls, message, song):
     try:
         await pycalls.stream(message.chat.id, song)
     except Exception as e:
-        await message.reply_text(f"Səsləndirilə bilmədi!\n\nXəta:\n{e}")
+        await message.reply_text(f"Səsləndirilə bilmədi!\n\nXəta:\n{e}")    
