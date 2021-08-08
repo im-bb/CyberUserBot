a0731370360b200b1aff58ccd314288bddbf48a0
whomiri <75207175+whomiri@users.noreply.github.com>
2021-08-08 19:52:26 +0400

    ADDED LOG READER FOR VERONICA (ASSISTENT)

--- userbot/events.py
+++ userbot/events.py
@@ -87,7 +87,7 @@ def register(**args):
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
                     eventtext = str(check.text)
-                    text = "LOG\n\n**C Y B Ξ R XƏTA BİLDİRİŞİ**\n"
+                    text = "**CYBΞRLOG**\n"
                     link = "@TheCyberSupport"
                     if len(eventtext)<10:
                         text += f"\n**⚙ Əmr:** {eventtext}\n"
