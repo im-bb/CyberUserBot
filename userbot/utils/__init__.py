8810f085063ea466bfd3705f2af33b8cec7ee5b9
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-08-19 11:51:09 +0400

    Update __init__.py

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -3,6 +3,6 @@
 # Telegram: @Faridxz
 
 from .chrome import chrome, options
-from .google_images_download import googleimagesdownload
+from .cyberimage import googleimagesdownload
 from .progress import progress
 from .tools import human_to_bytes, humanbytes, md5, time_formatter
