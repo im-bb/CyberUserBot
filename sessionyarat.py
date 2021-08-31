1edd7fec5e09912df32b84f65b1cd175a8aa0903
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-08-31 14:59:59 +0400

    Create sessionyarat.py

---
+++ sessionyarat.py
@@ -0,0 +1,20 @@
+import asyncio
+from pyrogram import Client
+
+async def main():
+    api_id = int(input("API ID: "))
+    api_hash = input("API HASH: ")
+    async with Client(":memory:", api_id=api_id, api_hash=api_hash) as app:
+        await app.send_message(
+            "me",
+            "**Cyber String**:\n\n"
+            f"`{await app.export_session_string()}`"
+        )
+        print(
+            "Təbriklər, String uğurla yaradıldı "
+            "String kayıtlı mesajlara göndərildi."
+        )
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
