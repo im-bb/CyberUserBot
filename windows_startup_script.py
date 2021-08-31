f06c00fe80852f6119bc4d978a35c17d20e4ff07
Færid <73734920+FaridDadashzade@users.noreply.github.com>
2021-08-31 14:38:21 +0400

    Delete windows_startup_script.py

--- windows_startup_script.py
+++
@@ -1,6 +0,0 @@
-from telethon import TelegramClient
-API_KEY="Bunu buraya yazın"
-API_HASH="Bunu buraya yazın."
-#my.telegram.org adresinden alın
-bot = TelegramClient('userbot',API_KEY,API_HASH)
-bot.start()
